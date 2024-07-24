03842fa0027579a89ed9fa634f1fafcff798e7ab
seondy <seondy99@gmail.com>
2024-07-24 21:47:25 +0900

    style: global, String 공용 파일 수정 #17

--- gamgyul-front/src/style/global.jsx
+++ gamgyul-front/src/style/global.jsx
@@ -1,21 +1,24 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-@import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css");
+  @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css");
     * {
-        padding: 0;
-        text-decoration: none;
-        margin: 0;
-        font-weight: 500;
+      padding: 0;
+      text-decoration: none;
+      margin: 0;
+      font-weight: 500;
 
-        /* 스크롤 안 보이게 하기 */
-        -ms-overflow-style: none; /* IE and Edge */
-        scrollbar-width: none; /* Firefox */
+      /* 스크롤 안 보이게 하기 */
+      -ms-overflow-style: none; /* IE and Edge */
+      scrollbar-width: none; /* Firefox */
 
-        /* 기본 고정 폰트 지정 */
-        font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;;
+      /* 기본 고정 폰트 지정 */
+      font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;;
     }
 
+    ul {
+      list-style: none;
+    }
     
     .layout {
       height: 100vh;
