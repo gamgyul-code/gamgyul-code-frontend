6fdd96161fe5128a46264cf28981944d10480e03
seondy <seondy99@gmail.com>
2024-08-01 16:35:45 +0900

    style: HomePage 퍼블리싱 #17

--- gamgyul-front/src/pages/HomePage/index.jsx
+++ gamgyul-front/src/pages/HomePage/index.jsx
@@ -10,15 +10,44 @@ const HomePage = () => {
   const language = "KR";
   const text = HOME_PAGE_TEXT[language];
 
+  /** 루트 & 관광지 리스트 클릭 */
+  const handleListClick = (props) => {
+    console.log(props);
+  };
+
+  /** 예시 데이터 */
+  const data = {
+    REGION_ATRCT: [
+      {
+        name: "여행루트이름1",
+        img: "이미지URL1",
+      },
+      {
+        name: "여행루트이름2",
+        img: "이미지URL2",
+      },
+      {
+        name: "여행루트이름3",
+        img: "이미지URL3",
+      },
+      {
+        name: "여행루트이름4",
+        img: "이미지URL4",
+      },
+    ],
+  };
+
   return (
     <>
       <HomeLayout>
         {/* 홈페이지의 상단 이미지 + 소개 내용 */}
         <StyledHomeHeader>
+          <img src="" alt="" />
           <Container>
-            <img src="" alt="" />
-            <p>{text.HEADER_MAIN}</p>
-            <p>{text.HEADER_SUB}</p>
+            <div>
+              <p>{text.HEADER_MAIN}</p>
+              <p>{text.HEADER_SUB}</p>
+            </div>
           </Container>
         </StyledHomeHeader>
         {/* 카테고리별 라우팅 (다른 페이지로 이동) */}
@@ -53,10 +82,11 @@ const HomePage = () => {
           <StyledCategoryName>{text.FOLKTALE_ROUTE}</StyledCategoryName>
           <nav>
             <ul>
-              <StyledLiRouter>여행루트이름1</StyledLiRouter>
-              <StyledLiRouter>여행루트이름2</StyledLiRouter>
-              <StyledLiRouter>여행루트이름3</StyledLiRouter>
-              <StyledLiRouter>여행루트이름4</StyledLiRouter>
+              {data.REGION_ATRCT.map((element, index) => {
+                return (
+                  <RouterLiItem key={`folktale-${index}`} data={element} onClick={() => handleListClick(element)} />
+                );
+              })}
             </ul>
           </nav>
         </StyledRouteAtrct>
@@ -65,10 +95,9 @@ const HomePage = () => {
           <StyledCategoryName>{text.REGION_ATRCT}</StyledCategoryName>
           <nav>
             <ul>
-              <StyledLiRouter>여행루트이름1</StyledLiRouter>
-              <StyledLiRouter>여행루트이름2</StyledLiRouter>
-              <StyledLiRouter>여행루트이름3</StyledLiRouter>
-              <StyledLiRouter>여행루트이름4</StyledLiRouter>
+              {data.REGION_ATRCT.map((element, index) => {
+                return <RouterLiItem key={`region-${index}`} data={element} onClick={() => handleListClick(element)} />;
+              })}
             </ul>
           </nav>
         </StyledRouteAtrct>
@@ -79,10 +108,73 @@ const HomePage = () => {
 
 export default HomePage;
 
+/** 각 라우터별 li 아이템 컴포넌트 (분리 필요) */
+const RouterLiItem = (props) => {
+  return (
+    <StyledLiRouter onClick={props.onClick}>
+      <img src="" alt={props.data.img} />
+      <p>{props.data.name}</p>
+    </StyledLiRouter>
+  );
+};
+
+/** 임시 카테고리 li태그 스타일링 (추후 컴포넌트화 예정) */
+const StyledLiRouter = styled.li`
+  width: 150px;
+  height: 216px;
+  border-radius: 24px;
+  background-color: ${theme.color.sub2};
+  flex: 0 0 auto;
+  overflow: hidden;
+
+  img {
+    width: 150px;
+    height: 150px;
+    display: block;
+    object-fit: cover;
+    background-color: black;
+  }
+
+  p {
+    font-size: ${theme.fontSize.body2};
+    color: ${theme.color.black};
+    width: calc(100% - 20px);
+    height: calc(100% - 168px);
+    padding: 9px 10px;
+  }
+`;
+
 const StyledHomeHeader = styled.header`
   width: 100%;
   height: 375px;
-  background-color: purple;
+  position: relative;
+  background-color: #dfefc6;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  div {
+    width: calc(100% - 40px);
+    position: absolute;
+    top: 24px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  div > p:first-child {
+    font-size: ${theme.fontSize.header};
+    color: ${theme.color.black};
+    line-height: 36px;
+    font-weight: 700;
+    white-space: pre-line;
+  }
+  div > p:nth-child(2) {
+    font-size: ${theme.fontSize.body3};
+    color: ${theme.color.gray1};
+    font-weight: 400;
+    margin-top: 8px;
+  }
 `;
 
 /** FOLKTALE_ROUTE, REGION_ATRCT Wrapper */
@@ -110,15 +202,6 @@ const StyledRouteAtrct = styled.section`
   }
 `;
 
-/** 임시 카테고리 li태그 스타일링 (추후 컴포넌트화 예정) */
-const StyledLiRouter = styled.li`
-  width: 150px;
-  height: 216px;
-  border-radius: 24px;
-  background-color: red;
-  flex: 0 0 auto;
-`;
-
 /** THEME_ATRCT Wrapper (h2 제외 따로 컴포넌트화 X) */
 const StyledThemeAtrct = styled.section`
   width: 100%;
@@ -139,7 +222,7 @@ const StyledThemeAtrct = styled.section`
     width: calc(50% - 7.5px);
     height: 95px;
     border-radius: 24px;
-    background-color: red;
+    background-color: black;
     overflow: hidden;
     position: relative;
   }
