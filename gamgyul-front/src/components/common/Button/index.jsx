9e213e174067309016a0ce7a4a91e67d183797c9
seondy <seondy99@gmail.com>
2024-08-13 21:33:50 +0900

    style: 공통 컴포넌트 Button Style 변경

--- gamgyul-front/src/components/common/Button/index.jsx
+++ gamgyul-front/src/components/common/Button/index.jsx
@@ -2,22 +2,31 @@ import styled from "styled-components";
 import { theme } from "../../../style/theme";
 import { applyFontStyles } from "../../../utils/fontStyles";
 
-const Button = ({ type, size, onClick, children, color, className, disabled, ...props }) => {
+const Button = ({ type, size, onClick, children, color, className, disabled, isShadow, ...props }) => {
   return (
-    <StyledButton type={type} size={size} onClick={onClick} color={color} className={className} disabled={disabled}>
+    <StyledButton
+      type={type}
+      size={size}
+      onClick={onClick}
+      color={color}
+      className={className}
+      disabled={disabled}
+      isShadow={isShadow}
+    >
       {children}
     </StyledButton>
   );
 };
 
 const StyledButton = styled.button`
-  ${applyFontStyles(theme.font.body1)}
+  ${applyFontStyles(theme.font.body2)}
   width: 100%;
-  height: ${({ type }) => (type === "small" ? "40px" : "64px")};
-  border-radius: 10px;
+  height: ${({ type }) => (type === "small" ? "42px" : "55px")};
+  border-radius: 20px;
   border: 0;
   cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
   background-color: ${({ disabled }) => (disabled ? theme.color.grayscale_BF : theme.color.primary)};
   color: ${theme.color.white};
+  box-shadow: ${({ isShadow }) => (isShadow ? "0px 2px 2px 0px #00000033" : "none")};
 `;
 export default Button;
