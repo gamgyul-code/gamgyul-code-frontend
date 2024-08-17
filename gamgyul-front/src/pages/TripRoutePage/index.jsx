9c27b345eb266607d03242ba5263378f39340207
seondy <seondy99@gmail.com>
2024-08-17 11:26:36 +0900

    feat: 5.2. 내 여행 경로 저장하기 팝업(모달) 연결 #28

--- gamgyul-front/src/pages/TripRoutePage/index.jsx
+++ gamgyul-front/src/pages/TripRoutePage/index.jsx
@@ -2,11 +2,13 @@ import styled from "styled-components";
 import { BasicLayout, Container } from "../../components/common/BasicLayout/layout.style";
 import BackNaviBtn from "../../components/common/BackNaviBtn";
 import { useState } from "react";
+import Modal from "../../components/common/Modal";
 
 const TripRoutePage = () => {
   const type = "USER_DEFINE"; // 사용자가 만든 경로
   //   const type = "PREDEFINE"; // 기존 제공 경로
   const [bookmark, setBookmark] = useState("off");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   /** 북마크 버튼 클릭 => 이후 api 요청 추가 */
   const handleBookmarkClick = (bookmark) => {
@@ -16,8 +18,21 @@ const TripRoutePage = () => {
       setBookmark("off");
     }
   };
+
+  /** 모달 닫기 */
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  /** 모달 확인 버튼 클릭 */
+  const handleModalCheck = (value) => {
+    // API 요청
+    console.log(value);
+  };
+
   return (
     <TripRouteLayout>
+      {isModalOpen && <Modal type="SAVE" onClick={handleModalCheck} onClose={handleCloseModal} />}
       <TripRouteContainer>
         <RouteMapSection>
           <BackNaviBtn />
@@ -36,7 +51,7 @@ const TripRoutePage = () => {
               <p>선택한 장소를 바탕으로 생성한 경로입니다.</p>
             </header>
             {type === "USER_DEFINE" ? (
-              <RouteSaveButton>
+              <RouteSaveButton onClick={() => setIsModalOpen(true)}>
                 <img src="/images/Icon/delete.svg" alt="" />
                 <span>저장하기</span>
               </RouteSaveButton>
