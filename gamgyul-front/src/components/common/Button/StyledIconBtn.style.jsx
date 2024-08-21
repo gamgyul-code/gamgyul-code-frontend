3ee581283c0959f27b9ee3d10e67a2dbac7d72e0
seondy <seondy99@gmail.com>
2024-08-21 18:35:09 +0900

    style: StyledIconBtn 24px 공통 스타일 분리

---
+++ gamgyul-front/src/components/common/Button/StyledIconBtn.style.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledIconBtn = styled.button`
+  width: 24px;
+  height: 24px;
+  border: none;
+  background-color: inherit;
+  cursor: pointer;
+`;
