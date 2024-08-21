bef069078dbba4651aa8a7ab284746a7de36a8fb
seondy <seondy99@gmail.com>
2024-08-21 18:36:12 +0900

    style: 5.2. 내 여행 경로 확인 페이지 퍼블리싱 #28

--- gamgyul-front/src/pages/TripRoutePage/index.jsx
+++ gamgyul-front/src/pages/TripRoutePage/index.jsx
@@ -5,12 +5,19 @@ import { useState } from "react";
 import Modal from "../../components/common/Modal";
 import { applyFontStyles } from "../../utils/fontStyles";
 import { theme } from "../../style/theme";
+import TripRouteItem from "../../components/common/TripRouteItem";
 
 const TripRoutePage = () => {
   const type = "USER_DEFINE"; // 사용자가 만든 경로
   //   const type = "PREDEFINE"; // 기존 제공 경로
   const [bookmark, setBookmark] = useState("off");
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [activeRoute, setActiveRoute] = useState(null);
+
+  const handleRouteClick = (index) => {
+    console.log(index);
+    setActiveRoute(index);
+  };
 
   /** 북마크 버튼 클릭 => 이후 api 요청 추가 */
   const handleBookmarkClick = (bookmark) => {
@@ -32,6 +39,13 @@ const TripRoutePage = () => {
     console.log(value);
   };
 
+  // 임시 데이터
+  const datas = [
+    { title: "제주 국제 공항", subtitle: "제주 시내" },
+    { title: "제주 국제 공항", subtitle: "제주 시내" },
+    { title: "제주 국제 공항", subtitle: "제주 시내" },
+    { title: "제주 국제 공항", subtitle: "제주 시내" },
+  ];
   return (
     <TripRouteLayout>
       {isModalOpen && <Modal type="SAVE" onClick={handleModalCheck} onClose={handleCloseModal} />}
@@ -65,12 +79,25 @@ const TripRoutePage = () => {
               </RouteBookmarkButton>
             )}
           </BottomSheetInfoContainer>
-          <section>
-            <div>경로 컴포넌트</div>
-            <div>경로 컴포넌트</div>
-            <div>경로 컴포넌트</div>
-            <div>경로 컴포넌트</div>
-          </section>
+          <BottomSheetRouteSection>
+            <nav>
+              <ul>
+                {datas.map((data, index) => {
+                  return (
+                    <TripRouteItem
+                      key={index}
+                      isFirst={index === 0}
+                      isLast={index === datas.length - 1}
+                      stepNumber={index + 1}
+                      data={data}
+                      isActive={index === activeRoute}
+                      onClick={() => handleRouteClick(index)}
+                    />
+                  );
+                })}
+              </ul>
+            </nav>
+          </BottomSheetRouteSection>
         </BottomSheetMain>
       </TripRouteContainer>
     </TripRouteLayout>
@@ -80,6 +107,7 @@ const TripRoutePage = () => {
 const TripRouteLayout = styled(BasicLayout)`
   padding: 0;
   height: 100vh;
+  position: relative;
 `;
 
 const TripRouteContainer = styled.div``;
@@ -87,22 +115,28 @@ const TripRouteContainer = styled.div``;
 /** Route 페이지의 상단 (Map API 들어가는 부분) */
 const RouteMapSection = styled.section`
   width: 100%;
-  height: 360px;
+  height: 390px;
+  background-color: skyblue;
   position: relative;
-  background-color: green;
 `;
 
 /** Route 페이지 하단시트 (경로) */
 const BottomSheetMain = styled.main`
-  background-color: red;
+  box-shadow: 0px 0px 10px 0px #0000001a;
+  background-color: ${theme.color.background};
   height: calc(100vh - 360px);
   border-radius: 30px 30px 0px 0px;
+  position: absolute;
+  top: 360px;
+  left: 0;
+  right: 0;
 `;
 
 /** 하단시트 정보 (header + 버튼) */
 const BottomSheetInfoContainer = styled(Container)`
   display: flex;
   justify-content: space-between;
+  margin-top: 32px;
 
   header > h2 {
     ${applyFontStyles(theme.font.subtitle)}
@@ -112,6 +146,13 @@ const BottomSheetInfoContainer = styled(Container)`
     ${applyFontStyles(theme.font.body3)}
     color: ${theme.color.gray1};
   }
+`;
+
+const BottomSheetRouteSection = styled.section`
+  width: 100%;
+  height: calc(100% - 114px);
+  margin-top: 24px;
+  overflow-y: scroll;
 `;
 
 /** 네비 연동 버튼 */
@@ -125,7 +166,7 @@ const NavLinkButton = styled.button`
   align-items: center;
   position: absolute;
   right: 20px;
-  bottom: 16px;
+  bottom: 46px;
   cursor: pointer;
 
   img {
@@ -141,6 +182,7 @@ const NavLinkButton = styled.button`
 
 /** 경로 저장 버튼 */
 const RouteSaveButton = styled.button`
+  width: 45px;
   display: flex;
   flex-direction: column;
   align-items: center;
