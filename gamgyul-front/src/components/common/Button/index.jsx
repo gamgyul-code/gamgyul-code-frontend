260630d23c926dc4028b1ea17c4c04650bd2f947
seondy <seondy99@gmail.com>
2025-01-03 20:33:11 +0900

    style: 공통 컴포넌트 Button 색상 추가

--- gamgyul-front/src/components/common/Button/index.jsx
+++ gamgyul-front/src/components/common/Button/index.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { theme } from "../../../style/theme";
 import { applyFontStyles } from "../../../utils/fontStyles";
 
-const Button = ({ type, size, onClick, children, color, className, disabled, isShadow, ...props }) => {
+const Button = ({ type, size, onClick, children, color, className, disabled, isShadow, isIcon, ...props }) => {
   return (
     <StyledButton
       type={type}
@@ -12,6 +12,7 @@ const Button = ({ type, size, onClick, children, color, className, disabled, isS
       className={className}
       disabled={disabled}
       isShadow={isShadow}
+      isIcon={isIcon}
     >
       {children}
     </StyledButton>
@@ -25,9 +26,33 @@ const StyledButton = styled.button`
   border-radius: ${({ type }) => (type === "small" ? "10px" : "20px")};
   border: 0;
   cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
-  background-color: ${({ disabled, color }) =>
-    disabled ? theme.color.gray2 : color === "gray" ? theme.color.gray4 : theme.color.primary};
-  color: ${({ color }) => (color === "gray" ? theme.color.gray2 : theme.color.white)};
+
+  background-color: ${({ disabled, color }) => {
+    if (color && color.includes("sub")) {
+      return disabled ? theme.color.sub3 : theme.color[color];
+    }
+    if (color === "none") {
+      return "inherit";
+    }
+    return disabled ? theme.color.gray2 : color === "gray" ? theme.color.gray4 : theme.color.primary;
+  }};
+
+  color: ${({ disabled, color }) => {
+    if (color && color.includes("sub")) {
+      return disabled ? theme.color.gray2 : theme.color.gray1;
+    }
+    if (color === "none") {
+      return theme.color.primary;
+    }
+    return color === "gray" ? theme.color.gray2 : theme.color.white;
+  }};
   box-shadow: ${({ isShadow }) => (isShadow ? "0px 2px 2px 0px #00000033" : "none")};
+  ${({ isIcon }) =>
+    isIcon &&
+    `
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    `}
 `;
 export default Button;
